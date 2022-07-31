236282fff322c4e28d8a7a2b9f19d8c6129c79c6
Olesia Ivanova <ivanova.ov_87@mail.ru>
2022-07-31 11:45:34 +0300

    мой второй коммит второго урока

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let num = 266219;
 let arr = num.toString().split('');
 let multiplies = (arr.reduce((a,b) => a*b))**3;
-//let result = multiplies**3;
 let result = multiplies.toString().split('')
 let end = result.slice(0, 2);
 
