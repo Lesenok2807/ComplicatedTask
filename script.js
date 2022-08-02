3373a036e256d668720e4203f5d6875a95443188
Olesia Ivanova <ivanova.ov_87@mail.ru>
2022-08-02 21:57:15 +0300

    первый коммит третьего урока

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-let num = 266219;
-let arr = num.toString().split('');
-let multiplies = (arr.reduce((a,b) => a*b))**3;
-let result = multiplies.toString().split('')
-let end = result.slice(0, 2);
+let ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let lang = en;
+
+if (lang == en) {
+    console.log(en.join(' '));
+} else {
+    console.log(ru.join(' '));
+}
+
+switch (lang) {
+    case en:
+        console.log(en.join(' '));
+       break;
+    case ru:
+        console.log(ru.join(' '));
+        break;
+    default:
+        console.log('Что- то пошло не так');
+}
+
+lang == en ? console.log(en.join(' ')) : console.log(ru.join(' '));
 
 
-console.log(end.join(''));
+let namePerson = 'Артем';
+namePerson == 'Артем' ? console.log('ДИРЕКТОР') : console.log('СТУДЕНТ');
+namePerson == 'Александр' ? console.log('ПРЕПОДАВАТЕЛЬ') : console.log('СТУДЕНТ');
