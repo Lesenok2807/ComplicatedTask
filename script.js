b4acf3279789221a1f2b4362329985f679dd0e2c
Olesia Ivanova <ivanova.ov_87@mail.ru>
2022-08-04 13:09:10 +0300

    первый коммит четвертого урока

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-let ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-let lang = en;
+const str = ' Свобода - это выбор и его нужно делать ежедневно';
 
-if (lang == en) {
-    console.log(en.join(' '));
-} else {
-    console.log(ru.join(' '));
-}
+const changeStr = function(str) {    
 
-switch (lang) {
-    case en:
-        console.log(en.join(' '));
-       break;
-    case ru:
-        console.log(ru.join(' '));
-        break;
-    default:
-        console.log('Что- то пошло не так');
-}
+    const checkString = function() {
 
-lang == en ? console.log(en.join(' ')) : console.log(ru.join(' '));
+        if (typeof str != 'string') {
+            return 'str - не строка';
+         } else {
+            return 'str - строка';
+        }
+    };
 
+    const deleteItem = function() {  
 
-let namePerson = 'Артем';
-namePerson == 'Артем' ? console.log('ДИРЕКТОР') : console.log('СТУДЕНТ');
-namePerson == 'Александр' ? console.log('ПРЕПОДАВАТЕЛЬ') : console.log('СТУДЕНТ');
+        const firstString = str.slice(1);
+        const callback = delElem();
+
+        function delElem() {        
+            if (str[0] !== ' ') {
+                return str;
+            } else {
+                return firstString;
+            }
+        } 
+          
+
+        if (str.charAt(str.length - 1) !== ' ') {
+            return callback;
+        } else {
+            return callback.slice(0, str.length - 1);
+        }
+    };    
+
+    const deleteElement = function() {
+        if (str.length > 30) {
+            return str.slice(0, str.length - 31).concat('(...)');
+        }
+    };  
+
+    console.log(checkString());
+    console.log(deleteItem());
+    console.log(deleteElement());
+  
+};
+
+changeStr(str);
